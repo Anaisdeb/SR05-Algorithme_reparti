549f2c9ded8839b36c273ca635b5b346cedd50de
Romain de Laage <romain.delaage@rdelaage.ovh>
2022-04-23 15:31:14 +0200

    Implements some messages class & vectorial clock

---
+++ utils.py
@@ -0,0 +1,19 @@
+class VectClock:
+    def __init__(self, i, N, tab):
+        self.i = i
+        self.N = N
+        self.tab = tab
+
+    def incr(self, otherClock):
+        self.tab[i] += 1
+        for j in range(N):
+            self.tab[j] = max(self.tab[j], otherClock.tab[j])
+
+    def __str__(self):
+        strTab = "#".join(map(str, self.tab))
+        return f"{self.i}#{self.N}#{strTab}"
+
+    @classmethod
+    def from_string(cls, s):
+        content = s.split("#")
+        return VectClock(content[0], content[1], content[2:])
